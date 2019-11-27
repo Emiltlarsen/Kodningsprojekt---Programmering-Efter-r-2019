ad60bc547b8d2fda61e98732cf381664173a66fa
Emiltlarsen <emil.t.larsen@gmail.com>
2019-11-27 13:03:45 +0100

    Update sim.js

--- sim.js
+++ sim.js
@@ -5,6 +5,9 @@ var TYNGDE_KRAFT_JORDEN = 9.8 * 1^2;     //Fortæller hvor hurtigt genstanden sk
 var MODSTAND_JORDEN = 1;   //Angiver aftagelsen af hastigheden når genstanden bevæger sig
 
 var N = 0;
+var timelapsed = 0;
+var distancetraveled = 0;
+var acc = distancetraveled / timelapsed^2
 
 var genstand = {
     m_bold: 100,
@@ -23,10 +26,16 @@ function draw(){
     background(200);
     ellipse(56, 46, 55, 55);
 
-    N = genstand.m_bold * TYNGDE_KRAFT_JORDEN/5^2
+    timelapsed = timelapsed + deltaTime/1000
+    distancetraveled = timelapsed * 9,8
+    
+    N = genstand.m_bold * (TYNGDE_KRAFT_JORDEN/deltaTime^2)
 
     textSize(32);
     text("Newton = " + N, 50, 350);
     fill(0, 100, 200)
+
+    if ((frameCount %30)==0) print(distancetraveled);
+
 }
-// N = m*a(m/s)
+// N = m*a(meter/s^2)
