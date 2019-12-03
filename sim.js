57d9f7aa8376a243f447704f54ca7c2291deb4ab
Emiltlarsen <emil.t.larsen@gmail.com>
2019-12-03 15:06:46 +0100

    Update sim.js

--- sim.js
+++ sim.js
@@ -2,19 +2,29 @@ var BREDDE = 800;   //Bredden af canvasset
 var HOJDE = 400;    //Højden af canvasset
 
 var TYNGDE_KRAFT_JORDEN = 9.82;     //Fortæller jordens tyngdekraft
-var MODSTAND_JORDEN = 1;   //Angiver aftagelsen af hastigheden når genstanden bevæger sig
+var Luft_rho = 1.225;   //Angiver aftagelsen af hastigheden når genstanden bevæger sig
+var Drag = 18*10^-6;
 
-var N = 0;
+var N_genstand = 0;
+var N_vind = 0;
 var timelapsed = 0;
 var d = 0;
-var acc = 0
+var acc = TYNGDE_KRAFT_JORDEN;
+var v = 0;
 
-//d=1/2*g*t^2 
-//v=g*t
-var genstand = {
+
+var masse = {
     m_bold: 100,
     m_kasse: 0,
     }
+
+var areal = {
+    A_faldskærm: 10,
+    A_bold: 0.5,
+    A_kasse: 0,
+
+}
+
 
 
 function setup() {
@@ -29,25 +39,28 @@ function draw(){
     ellipse(BREDDE/2, HOJDE/2, 55, 55);
 
     
-timelapsed = timelapsed + 1/30
-    /*
-    distancetraveled = timelapsed * 9,8
-    
-    N = genstand.m_bold * acc//(TYNGDE_KRAFT_JORDEN/timelapsed^2)
-    */
-    acc = (1/2)*TYNGDE_KRAFT_JORDEN / timelapsed^2
-    d=1/2*TYNGDE_KRAFT_JORDEN*timelapsed^2
-    v=TYNGDE_KRAFT_JORDEN*timelapsed
+    timelapsed = timelapsed + 1/30;
+
+
+    N_genstand = masse.m_bold * acc;
+        //N=m*a
+    d=1/2*(TYNGDE_KRAFT_JORDEN-0)*(timelapsed*timelapsed);
+        //d=1/2*g*t^2 
+    v=TYNGDE_KRAFT_JORDEN*timelapsed;
+        //v=g*t
+    N_v=1/2*Luft_rho*(areal.A_bold+areal.A_faldskærm)*(v*v)*Drag;
+
     
     textSize(32);
-    text("Tid gået = " + timelapsed, 50, 225);
-    text("Newton = " + N, 50, 275);
-    text("Distance = " + d, 50, 325);
-    text("Hastighed = " + v, 50, 375);
+    text("Tid gået = " + timelapsed, 50, 175);
+    text("Newton = " + N_genstand, 50, 225);
+    text("Distance = " + d, 50, 275);
+    text("Hastighed = " + v, 50, 325);
+    text("Acceleration = " + acc, 50, 375);
+    text("Modstand = " + N_v, 50, 125);
 
-    fill(0, 100, 200)
+    fill(0, 100, 200);
 
     if ((frameCount %30)==0) print(d);
 
 }
-// N = m*a(meter/s^2)
