22ca489888957d1bda38ed104a73a50b26eba02d
Emiltlarsen <emil.t.larsen@gmail.com>
2019-12-03 13:34:47 +0100

    Update sim.js

--- sim.js
+++ sim.js
@@ -1,42 +1,44 @@
 var BREDDE = 800;   //Bredden af canvasset
 var HOJDE = 400;    //Højden af canvasset
 
-var TYNGDE_KRAFT_JORDEN = 9.82;     //Fortæller hvor hurtigt genstanden skal falde
+var TYNGDE_KRAFT_JORDEN = 9.82;     //Fortæller jordens tyngdekraft
 var MODSTAND_JORDEN = 1;   //Angiver aftagelsen af hastigheden når genstanden bevæger sig
 
 var N = 0;
 var timelapsed = 0;
-var distancetraveled = 0;
-var acc = (1/2)*TYNGDE_KRAFT_JORDEN / timelapsed^2
+var d = 0;
+var acc = 0
 
 //d=1/2*g*t^2 
 //v=g*t
 var genstand = {
     m_bold: 100,
-
+    m_kasse: 0,
     }
 
 
 function setup() {
     frameRate(30);
-    createCanvas(500, 400);
+    createCanvas(BREDDE, HOJDE);
 
 
 }
 
 function draw(){
     background(200);
-    ellipse(56, 46, 55, 55);
+    ellipse(BREDDE/2, HOJDE/2, 55, 55);
 
     
-    timelapsed = timelapsed + deltaTime/1000
+    timelapsed = timelapsed + 0.0333333
     /*
     distancetraveled = timelapsed * 9,8
     
     N = genstand.m_bold * acc//(TYNGDE_KRAFT_JORDEN/timelapsed^2)
     */
+    acc = (1/2)*TYNGDE_KRAFT_JORDEN / timelapsed^2
     d=1/2*TYNGDE_KRAFT_JORDEN*timelapsed^2
     v=TYNGDE_KRAFT_JORDEN*timelapsed
+    
     textSize(32);
     text("Tid gået = " + timelapsed, 50, 225);
     text("Newton = " + N, 50, 275);
@@ -45,7 +47,7 @@ function draw(){
 
     fill(0, 100, 200)
 
-    if ((frameCount %30)==0) print(distancetraveled);
+    if ((frameCount %30)==0) print(d);
 
 }
 // N = m*a(meter/s^2)
